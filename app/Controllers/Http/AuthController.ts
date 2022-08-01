6f9993e3914a51eabca254921a5f12a359fd3844
Ashish <ashsihbhalray865@gmail.com>
2022-08-01 10:21:16 +0530

    Intial Commmit

---
+++ app/Controllers/Http/AuthController.ts
@@ -0,0 +1,46 @@
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { schema, rules } from '@ioc:Adonis/Core/Validator'
+import User from 'App/Models/user'
+
+
+export default class AuthController {
+    //registration function
+    public async register({request}:HttpContextContract){
+    const validate = 
+    {schema:schema.create({
+        email: schema.string([
+            rules.email()
+          ]),
+          password: schema.string([
+            rules.confirmed(),
+            rules.minLength(4)
+          ])
+    }),
+    messages: {
+      required: 'The {{ field }} is required to create a new account',
+      'email.unique': 'email not available'
+    }
+  }
+    
+    const data  = await request.validate(validate)
+    const user = await User.create(data)
+    return user
+
+    }
+
+    //login function
+    public async login({request ,auth , response}:HttpContextContract){
+     
+        const email = request.input('email')
+        const password = request.input('password')
+
+  try {
+     const token = await auth.attempt(email, password)
+     return token.toJSON()
+  } catch {
+    return response.unauthorized('Invalid credentials')
+  }
+
+
+    }
+}
